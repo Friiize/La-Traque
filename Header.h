fe3ac1e2ce51ba55eeccc1974c2e27d76a422256
Frize <raffaele.leruste@gmail.com>
2020-12-07 17:21:06 +0100

    Affichage du tableau

---
+++ Header.h
@@ -0,0 +1,41 @@
+#pragma once
+#pragma warning (disable:4996);
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void eventHandler();
+
+typedef struct pisteur {
+	int x = 0;
+	int y = 0;
+	int detectArea[8][2] = {
+		{x--, y--},
+		{x, y--},
+		{x++, y--},
+		{x--, y},
+		{x++, y},
+		{x--, y++},
+		{x, y++},
+		{x++, y++},
+	};
+
+	int up = 0;
+	int down = 0;
+	int left = 0;
+	int right = 0;
+	int isDead = 0;
+};
+
+typedef struct monstre {
+	int x = 0;
+	int y = 0;
+	int hp = 4;
+
+	int prevPos[15][3] = { NULL };
+
+	int up = 0;
+	int down = 0;
+	int left = 0;
+	int right = 0;
+};
