2f96402ebbba57cbd06ae907816673ae494e4a57
Frize <raffaele.leruste@gmail.com>
2020-12-07 18:07:15 +0100

    gestion pisteurs (en construction)

--- La Traque/La Traque/Source.cpp
+++ La Traque/La Traque/Source.cpp
@@ -3,6 +3,8 @@
 const int W = 31;
 const int H = 16;
 
+
+
 void drawScreen(char screen[][W], const int H, const int W) {
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
@@ -10,6 +12,23 @@ void drawScreen(char screen[][W], const int H, const int W) {
 		}
 		printf("\n");
 	}
+}
+
+pisteur* initPisteur(char screen[][W], const int H, const int W) {
+	int input = -1;
+
+	do {
+		system("cls");
+		drawScreen(screen, H, W);
+		printf("Combien de pisteur ? (entre 3 et 10)\n");
+		scanf("%d", &input);
+	} while (input < 3 || input > 10);
+
+	return (pisteur*)calloc(input, sizeof(pisteur));
+}
+
+void posPisteur(pisteur* pisteurs) {
+
 }
 
 void main() {
@@ -25,18 +44,21 @@ void main() {
 			else { 
 				screen[i][j] = ' '; 
 			}
-
 		}
-	}
+	}//Initialise le tableau
 
 	drawScreen(screen, H, W);
 	printf("Bienvenue dans La Traque !\n\n\n");
 
 	system("pause");
+	system("cls");
+
+	pisteurs = initPisteur(screen, H, W);//Initialise le nombre de pisteur
+	posPisteur(pisteurs);
 
 	while (quit) {
 		system("cls");
 		drawScreen(screen, H, W);
-
+		system("pause"); //Temporaire
 	}
 }
