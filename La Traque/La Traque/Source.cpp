46a123b22ee91e1e247dcf3cfdcf518d7642f76d
Frize <raffaele.leruste@gmail.com>
2020-12-08 16:25:22 +0100

    refactorisation du code

--- La Traque/La Traque/Source.cpp
+++ La Traque/La Traque/Source.cpp
@@ -3,6 +3,7 @@
 const int W = 31;
 const int H = 16;
 
+
 void drawScreen(char screen[][W], const int H, const int W) {
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
@@ -12,10 +13,8 @@ void drawScreen(char screen[][W], const int H, const int W) {
 	}
 }
 
-pisteur* initPisteur(char screen[][W], pisteur* pisteurs, const int H, const int W) {
+pisteur* nbPisteur(char screen[][W], pisteur* pisteurs, const int H, const int W) {
 	int input = -1;
-	int x = -1;
-	int y = -1;
 
 	do {
 		system("cls");
@@ -24,9 +23,14 @@ pisteur* initPisteur(char screen[][W], pisteur* pisteurs, const int H, const int
 		scanf("%d", &input);
 	} while (input < 3 || input > 10);
 
-	pisteurs = (pisteur*)calloc(input, sizeof(pisteur));
+	return (pisteur*)realloc(pisteurs, input * sizeof(pisteur));
+}
 
-	for (int i = 0; i < input; i++) {
+void posPisteur(pisteur* pisteurs, char screen[][W], int W, int H) {
+	int x = -1;
+	int y = -1;
+
+	for (int i = 0; i < sizeof(pisteurs); i++) {
 		do {
 			system("cls");
 			drawScreen(screen, H, W);
@@ -37,26 +41,30 @@ pisteur* initPisteur(char screen[][W], pisteur* pisteurs, const int H, const int
 
 			for (int j = 0; j < i; j++) {
 				printf("Ca passe");
-				if ((&pisteurs[j].x == &x) && (&pisteurs[j].y == &y)) {
+				system("pause");
+
+				if ((pisteurs[j].x == x) && (pisteurs[j].y == y)) {
 					system("cls");
 					drawScreen(screen, H, W);
 					printf("Un pisteur est déjà placé ici. (X : %d, Y : %d)", x, y);
-					j = input;//On forces la fin de la boucle
+					j = i;//On forces la fin de la boucle
+
 				}//Message d'erreur si le joueur place un pisteur sur un autre pisteur
+
 			}//Boucle autant de fois que de positions attribués
-		} while ((x == 0 || x == H - 1) || (y == 0 || y == W - 1) && ((&pisteurs[i].x == &x) && (&pisteurs[i].y == &y)));
+
+		} while ((x == 0 || x == H - 1) || (y == 0 || y == W - 1) && ((pisteurs[i].x == x) && (pisteurs[i].y == y)));
 
 		screen[y][x] = 'P';
 	}//On demande au joueur les positions de ces pisteurs
-
-	return pisteurs;
 }
 
 void main() {
 	char screen[H][W] = { NULL };
 	int quit = 1;
-	pisteur* pisteurs = { NULL };
+	pisteur* pisteurs = (pisteur*)calloc(1, sizeof(pisteur));
 
+	//Initialise le tableau
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
 			if (i == 0 || i == H-1 || j == 0 || j == W-1) {
@@ -66,19 +74,27 @@ void main() {
 				screen[i][j] = ' '; 
 			}
 		}
-	}//Initialise le tableau
+	} 
 
+	//Premire affichage du jeu
 	drawScreen(screen, H, W);
 	printf("Bienvenue dans La Traque !\n\n\n");
-
 	system("pause");
 	system("cls");
 
-	pisteurs = initPisteur(screen, pisteurs, H, W);//Initialise le nombre de pisteur
+	pisteurs = nbPisteur(screen, pisteurs, H, W); //Initialise le nombre de pisteur
+	posPisteur(pisteurs, screen, W, H);//Gére l'emplacement des pisteurs
 
 	while (quit) {
 		system("cls");
 		drawScreen(screen, H, W);
 		system("pause"); //Temporaire
+
+		for (int i = 0; i < sizeof(pisteurs); i++) {
+			quit = 0;
+			if (pisteurs[i].isDead == 0) {
+				quit = 1;
+			}
+		}//check si tout les pisteurs sont mort, si oui, le jeu s'arrêtes
 	}
 }
